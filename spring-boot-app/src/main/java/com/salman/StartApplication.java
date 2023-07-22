92e4e69eeaa11b3a07ee56e60ca450e7e695381d
imsalmanmalik <snmalik98@gmail.com>
2023-07-22 20:06:19 +0500

    first commit

---
+++ spring-boot-app/src/main/java/com/salman/StartApplication.java
@@ -0,0 +1,24 @@
+package com.salman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class StartApplication {
+
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "This application is deployed on to Kubernetes using Argo CD");
+        model.addAttribute("msg", "Salman has successfuly built a sprint boot application using Maven");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
